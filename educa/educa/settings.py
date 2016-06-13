94e951ad7d114bdad7566e7319d9d1dd8600f034
Ujjawal <ujjawal22malik@gmail.com>
2016-06-13 18:35:30 +0530

    Big commit : Added Module and Content update views , urls and templates. Also fixed the redirect url for login.

--- educa/educa/settings.py
+++ educa/educa/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/1.9/ref/settings/
 """
 
 import os
+from django.core.urlresolvers import reverse_lazy
+
+LOGIN_REDIRECT_URL = reverse_lazy('manage_course_list')
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -31,13 +34,14 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'courses',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'courses',
+
 ]
 
 MIDDLEWARE_CLASSES = [
